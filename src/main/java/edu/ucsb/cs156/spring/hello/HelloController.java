355984372e6b17a0c058c91c41f58fdcea9c21e6
Joseph Ng <joseph_ng@ucsb.edu>
2022-04-06 17:57:55 -0700

    fixed small typo

--- src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from joseph_ng!";
+        return "Greetings from joseph-ng!";
     }
     
 }
